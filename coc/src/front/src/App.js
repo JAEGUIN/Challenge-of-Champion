4462b32d6c8e39830894fef3a21ccc4cf8a28dd1
park.u <pyj0724@gmail.com>
2024-01-26 10:34:34 +0900

    test

--- coc/src/front/src/App.js
+++ coc/src/front/src/App.js
@@ -1,13 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import MainPage from './main';
 import RegisterPage from './member/RegisterPage';
 import Login from './member/LoginPage';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<MainPage/>}/>
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Login />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
