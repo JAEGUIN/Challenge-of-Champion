1a24543194b40e69020a866d3ebbf25586bdfce3
skyum1 <rjftkd126@gmail.com>
2024-02-15 21:07:06 +0900

    240215_무한스크롤구현

--- COC_FE/src/component/Home.js
+++ COC_FE/src/component/Home.js
@@ -2,32 +2,65 @@ import React, { useState, useEffect } from 'react';
 import PopularArena from './PopularArena';
 import MainContents from './MainContents';
 import axios from 'axios';
+import { throttle } from 'lodash';
 
 const Home = () => {
     const [contentData, setContentData] = useState([]);
+    const [pageNum, setPageNum] = useState(0);
+    const [getData, setGetData] = useState(true);
 
     // 처음에 시작
+    const fetchData = async () => {
+        try {
+          const response = await axios.get('/board', {
+            params: {
+              pageNum: pageNum
+            }
+          });
+          if(response.data.length == 0) {
+            alert("더 이상 조회할 데이터가 없습니다.");
+            setGetData(false);
+          } else {
+            setContentData(prevData => [...prevData, ...response.data]);
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+    };
+
+    // 스크롤 이벤트 추가
+    const handleScroll = throttle(() => {
+        const scrollTop = document.documentElement.scrollTop;
+        const windowHeight = window.innerHeight;
+        const scrollHeight = document.documentElement.scrollHeight;
+        
+        // 스크롤이 바닥에 닿았을 때
+        if (scrollTop + windowHeight === scrollHeight && getData) {
+            console.log("바닥찍음!");
+            setPageNum(pageNum + 10);
+            fetchData();
+        } 
+    } , 300);
+
     useEffect(() => {
-        axios.get('/board')
-            .then(response => {
-                console.log(response);
-                setContentData(response.data);
-            });
+        fetchData();
     }, []);
 
-    // data받고 난 후
     useEffect(() => {
-        console.log(contentData);
-        for(const content of contentData) {
-            //console.log(content["content"]);
-        }
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [handleScroll]) ;
 
-    }, [contentData]);
+    
 
     return (
         <div>
             <PopularArena/>
-            <MainContents/>
+            <div class="mainContents">
+                {contentData.map((item) => (
+                    <MainContents data={item}/>
+                ))}
+            </div>
         </div>
     );
 };
--- COC_FE/src/component/MainContents.js
+++ COC_FE/src/component/MainContents.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const MainContents = (props) => {
@@ -10,18 +10,27 @@ const MainContents = (props) => {
         navigate('/detailContent');
     };
 
+    const [content, setContent] = useState("");
+    const [nickName, setNickName] = useState("");
+
+    useEffect(() => {
+        console.log(props);
+        setContent(props.data.content);
+        setNickName(props.data.nickname);
+    }, []);
+
     return (
         <div className="mainContent">
             {/* 프로필 부분 */}
             <div className="d-flex">
                 <img src="/img/sample.jpg" className="contentProfileImg"/>
                 <div className="profileDesc d-flex">
-                    <div className="profileName">대한민국</div>
+                    <div className="profileName">{nickName}</div>
                     <div className="contentDate right-align-container"> 2024/01/01 </div>
                 </div>
             </div>
             <div onClick={goDetailContent}>
-                <div>메밀꽃 필 무렵<br></br>여름장이란 애시당초 글러서 해는 아직 중천에 있건만 장판은 벌써 쓸쓸하고 더운 햇발이 벌려 놓은 전시장 밑으로 등줄기를 훅훅 볶는다. </div>
+                <div><br/>{content}</div>
                 <img src="/img/sampleContent.jpg" className="contentImg"/>
             </div>
         </div>
