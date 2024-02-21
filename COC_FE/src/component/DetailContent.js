bbb55b2de6d6c5e0835b6b6ebaaa70e5649c109b
skyum1 <rjftkd126@gmail.com>
2024-02-21 22:03:45 +0900

    240201_게시판상세 수정

--- COC_FE/src/component/DetailContent.js
+++ COC_FE/src/component/DetailContent.js
@@ -5,8 +5,12 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
 
 const DetailContent = () => {
-    const [contentData, setContentData] = useState('');
-    const [replyData, setReplyData] = useState([]);
+    const [contentView, setContentView] = useState(0); // 조회 수
+    const [heartCnt, setHeartCnt] = useState(0); // 좋아요 수
+    const [contentData, setContentData] = useState(''); // 컨텐츠 내용
+    const [replyData, setReplyData] = useState([]); // 답글
+    const [updatedAt, SetUpdatedAt] = useState(''); // 업데이트 날짜
+    const [nickName, setNickName] = useState(''); // 닉네임
     const location = useLocation();
     const navigate = useNavigate();
 
@@ -27,8 +31,24 @@ const DetailContent = () => {
             console.error('Error fetching data:', error);
         }
     };
+
+    // 날짜를 2024-01-01 형식으로
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+      
+        return `${year}-${month}-${day}`;
+    }
+
+
     useEffect(() => {
+        setNickName(location.state.boardData.data.nickname);
+        SetUpdatedAt(formatDate(location.state.boardData.data.updatedAt));
         setContentData(location.state.boardData.data.content);
+        setContentView(location.state.boardData.data.count);
+        setHeartCnt(location.state.boardData.data.heart);
         fetchData();
     }, []);
 
@@ -37,8 +57,8 @@ const DetailContent = () => {
             <div className="d-flex">
                 <img src="/img/sample.jpg" className="contentProfileImg" onClick={goProfil}/>
                 <div className="profileDesc d-flex">
-                    <div className="profileName" onClick={goProfil}>대한민국</div>
-                    <div className="contentDate right-align-container"> 2024/01/01 </div>
+                    <div className="profileName" onClick={goProfil}>{nickName}</div>
+                    <div className="contentDate right-align-container"> {updatedAt} </div>
                 </div>
             </div>
             <div>
@@ -48,13 +68,13 @@ const DetailContent = () => {
 
             <div className="d-flex detailCnt">
                 <div class="float-start detailLike">
-                    좋아요
+                    ❤ {heartCnt}
                 </div >
                 <div class="float-none detailComment">
-                    댓글
+                    댓글 : {replyData.length}
                 </div>
                 <div class="float-end detailView">
-                    조회 수
+                    조회 수 : {contentView}
                 </div>
             </div>
 
